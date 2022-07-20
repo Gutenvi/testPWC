97f66b1580a0a4054ba19f73f2165df609d86014
Gutenvi <alvaro.gutierrez.tenorio@gmail.com>
2022-07-20 17:45:45 +0200

    Commit towapo

---
+++ main.py
@@ -0,0 +1,17 @@
+def ander_a_currar():
+  """
+  Funcion que cuenta un chiste.
+  """
+  pass
+
+def jose_haz_algo():
+  """
+  Funcion que te insulta.
+  """
+  pass
+
+def victor_yoquese():
+  """
+  Funcion que hace cosas.
+  """
+  pass
